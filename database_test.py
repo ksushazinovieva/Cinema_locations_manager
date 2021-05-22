f39bc1fd6fb81f5f8c1a490f58e0b9bb4e9c7a83
Ksusha <80685905+ksushazinovieva@users.noreply.github.com>
2021-05-22 17:40:32 +0300

    Update database_test.py

--- database_test.py
+++ database_test.py
@@ -1,5 +1,4 @@
 import pyodbc
-import json
 
 connection_string = "Driver={ODBC Driver 17 for SQL Server};"\
 "Server=tcp:84.252.140.138;"\
@@ -11,6 +10,11 @@ conn = pyodbc.connect(connection_string)
 cursor = conn.cursor()
 
 cursor.execute("select * from TheLocals")
+for row in cursor:
+    print(row)
+conn.commit()
+
+cursor.execute("select * from LocationHunters")
 for row in cursor:
     print(row)
 conn.commit()
